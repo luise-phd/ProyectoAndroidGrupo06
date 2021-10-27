d8863c82e2fcd76eeeec8dd5ab34d72e3fc2bcdb
luise-phd <luis.ordonez.palacios@correounivalle.edu.co>
2021-10-26 19:38:36 -0500

    Initial commit

---
+++ app/src/main/java/com/ingluise/proyectoactivities/MainActivity.java
@@ -0,0 +1,64 @@
+package com.ingluise.proyectoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
+        Log.i("Información", "onCreate");
+    }
+
+    public void goToActivitySecond(View view) {
+        Intent newIntent = new Intent(this, SecondActivity.class);
+        newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        newIntent.putExtra("msg", "Hola");
+        newIntent.putExtra("year", 2021);
+        startActivity(newIntent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Información","onDestroy");
+    }
+}
---
+++ app/src/main/java/com/ingluise/proyectoactivities/SecondActivity.java
@@ -0,0 +1,31 @@
+package com.ingluise.proyectoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        getParametros();
+    }
+
+    public void getParametros() {
+        Bundle extras = getIntent().getExtras();
+        String msg = extras.getString("msg");
+        int year = extras.getInt("year");
+        Toast.makeText(this, msg + " " + year, Toast.LENGTH_SHORT).show();
+    }
+
+    public void goToActivityMain(View view) {
+        Intent newIntent = new Intent(this, MainActivity.class);
+        newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(newIntent);
+    }
+}
