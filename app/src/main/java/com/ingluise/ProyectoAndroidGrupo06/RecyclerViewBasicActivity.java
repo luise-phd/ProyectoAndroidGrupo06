f45c5929e2f2ff51fa61b449139c5d984f9e1f8f
luise-phd <luis.ordonez.palacios@correounivalle.edu.co>
2021-11-30 08:05:35 -0500

    RecyclerViewBasic

---
+++ app/src/main/java/com/ingluise/ProyectoAndroidGrupo06/RecyclerViewBasicActivity.java
@@ -0,0 +1,57 @@
+package com.ingluise.ProyectoAndroidGrupo06;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class RecyclerViewBasicActivity extends AppCompatActivity {
+    private final int tam = 100000;
+    private final int cols = 3;
+    private final int pos = 99999;
+
+    private CustomAdapter ca;
+
+    private LinearLayoutManager llm;
+    private GridLayoutManager glm;
+
+    private RecyclerView rv;
+
+    private String[] dataset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view_basic);
+
+        rv = findViewById(R.id.recyclerView);
+
+        initDataset();
+
+        ca = new CustomAdapter(dataset);
+        rv.setAdapter(ca);
+
+        setRecyclerViewLayoutManager();
+    }
+
+    private void initDataset() {
+        dataset = new String[tam];
+        for (int i=0; i < dataset.length; i++) {
+            dataset[i] = "Este es el elemento # " + i;
+        }
+    }
+
+    public void setRecyclerViewLayoutManager() {
+        int scrollPosition = pos;
+
+        llm = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        glm = new GridLayoutManager(this, cols);
+
+//        rv.setLayoutManager(llm);
+        rv.setLayoutManager(glm);
+        rv.scrollToPosition(scrollPosition);
+    }
+}
